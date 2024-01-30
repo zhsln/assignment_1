d9e8f5a5d644c4be79ea8107eb8d30149b7f8bf1
zhasu <230332@astanait.edu.kz>
2024-01-30 14:02:34 +0600

    Version 2.1
    Optimised the code.

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -8,36 +8,36 @@ public class Shape {
     public Shape() {
         pointArrayList = new ArrayList<>();
     }
+
     public void addPoint(Point point) {
         pointArrayList.add(point);
     }
 
     // Method to calculate the perimeter of the shape
     public double calculatePerimeter() {
-        double perimeter = 0;
+        double perimeter = pointArrayList.getLast().distanceTo(pointArrayList.getFirst()); // distance between last and first points
+
+        // distance between other points
         for (int i = 0; i < pointArrayList.size() - 1; i++) {
             perimeter += pointArrayList.get(i).distanceTo(pointArrayList.get(i + 1));
         }
-        // Add the distance from the last point to the first one (closing the shape)
-        perimeter += pointArrayList.get(pointArrayList.size() - 1).distanceTo(pointArrayList.get(0));
+
         return perimeter;
     }
+
     // Method to get the length of the longest side
     public double getLongestSide() {
-        double longestSide = 0;
+        double longestSide = pointArrayList.getLast().distanceTo(pointArrayList.getFirst()); // distance between last and first points
+
+        // distance between other points
         for (int i = 0; i < pointArrayList.size() - 1; i++) {
             double currentSide = pointArrayList.get(i).distanceTo(pointArrayList.get(i + 1));
-            if (currentSide > longestSide) {
-                longestSide = currentSide;
-            }
+            if (currentSide > longestSide) { longestSide = currentSide; }
         }
-        // Check the distance from the last point to the first one
-        double lastSide = pointArrayList.get(pointArrayList.size() - 1).distanceTo(pointArrayList.get(0));
-        if (lastSide > longestSide) {
-            longestSide = lastSide;
-        }
+
         return longestSide;
     }
+
     // Method to get the average length of the sides
     public double getAverageSide() { return calculatePerimeter() / pointArrayList.size(); }
 }
